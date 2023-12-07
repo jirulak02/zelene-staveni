cfd16d0becbadbd129e87d8f70aff2df20542c46
Jiří Šimeček <109624095+jirulak02@users.noreply.github.com>
2023-12-07 21:44:56 +0200

    change number of realizations

--- components/HomePage/Achievments.tsx
+++ components/HomePage/Achievments.tsx
@@ -30,7 +30,7 @@ export default function Achievments() {
           />
         </div>
         <div className="space-y-2 px-3 text-center sm:space-y-3">
-          <Counter countTo={100} className="text-6xl text-zelena sm:text-7xl" />
+          <Counter countTo={213} className="text-6xl text-zelena sm:text-7xl" />
           <h3 className="text-zelena">REALIZACÍ</h3>
           <p>Cenovou nabídku vypracujeme zdarma.</p>
         </div>
