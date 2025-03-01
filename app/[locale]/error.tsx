f3fd5371c6474ce0a8d072eb635559269d527ae9
jirulak02 <jirulak02@gmail.com>
2025-03-01 20:08:55 +0100

    fix: not found trigger

--- app/[locale]/error.tsx
+++ app/[locale]/error.tsx
@@ -10,15 +10,17 @@ export default function Error({ error, reset }: { error: Error; reset: () => voi
   log.error("Error caught in app/error.tsx", error);
 
   return (
-    <div className="absolute top-0 z-50 flex h-full w-full flex-col justify-center space-y-3 bg-neutral-100 p-3 text-center">
-      <h2 className="font-semibold">{t("title")}</h2>
-      <button
-        className="m-auto rounded-3xl bg-zelena px-4 py-2 text-neutral-100 hover:bg-neutral-800 md:px-5 md:py-3"
-        onClick={reset}
-      >
-        {t("button.label")}
-      </button>
-      <p>{t("message")}</p>
+    <div className="absolute top-0 z-50 flex h-full w-full flex-col justify-center text-center bg-neutral-100 p-3">
+      <div className="flex flex-col gap-4">
+        <h2 className="font-semibold">{t("title")}</h2>
+        <button
+          className="m-auto rounded-3xl bg-zelena px-4 py-2 text-neutral-100 hover:bg-neutral-800 md:px-5 md:py-3"
+          onClick={reset}
+        >
+          {t("button.label")}
+        </button>
+        <p>{t("message")}</p>
+      </div>
     </div>
   );
 }
